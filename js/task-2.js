62bf8c2871dff12e5a68ae0286e398424836ea47
dima1310 <dimaponomarenko13101981@gmail.com>
2025-03-29 21:10:20 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,35 @@
-const user = {
-  firstName: "dima",
-  lastName: "ponomarenko",
-  age: 43,
-};
-console.log(user);
+function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
 
-user.age = 42;
+  const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
 
-user.job = "developer";
+  return totalCalories / days.length;
+}
+
+// Перевірка коректності роботи функції
+console.log(
+  calcAverageCalories([
+    { day: "Monday", calories: 3010 },
+    { day: "Tuesday", calories: 3200 },
+    { day: "Wednesday", calories: 3120 },
+    { day: "Thursday", calories: 2900 },
+    { day: "Friday", calories: 3450 },
+    { day: "Saturday", calories: 3280 },
+    { day: "Sunday", calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+console.log(calcAverageCalories([])); // 0
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,25 @@
-const user = {
-  firstName: "dima",
-  lastName: "ponomarenko",
-  age: 43,
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 
-for (const key in user) {
-  console.log(`${key}: ${user[key]}`);
-}
+// Перевірка роботи методів
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Michael has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Michael has 320 active hours!"
